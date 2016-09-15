399ec2cf1a9b89c61e3cc26670cd9e3be2df3724
mzyil <mzyil12@gmail.com>
2016-09-16 02:21:40 +0300

    tweak from interface, draw around the marks

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -29,16 +29,27 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.tbThresholdMin = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbThresholdMax = new System.Windows.Forms.TextBox();
+            this.tbCannyThreshold = new System.Windows.Forms.TextBox();
+            this.tbCannyThresholdLinking = new System.Windows.Forms.TextBox();
+            this.labelx = new System.Windows.Forms.Label();
+            this.labely = new System.Windows.Forms.Label();
+            this.tbMinOuterRectArea = new System.Windows.Forms.TextBox();
+            this.tbMaxMarkArea = new System.Windows.Forms.TextBox();
+            this.labelz = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(18, 22);
+            this.button1.Location = new System.Drawing.Point(726, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(96, 34);
             this.button1.TabIndex = 0;
@@ -46,17 +57,17 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // textBox1
+            // tbThresholdMin
             // 
-            this.textBox1.Location = new System.Drawing.Point(140, 28);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(140, 20);
-            this.textBox1.TabIndex = 2;
+            this.tbThresholdMin.Location = new System.Drawing.Point(90, 12);
+            this.tbThresholdMin.Name = "tbThresholdMin";
+            this.tbThresholdMin.Size = new System.Drawing.Size(96, 20);
+            this.tbThresholdMin.TabIndex = 2;
             // 
             // pictureBox1
             // 
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 62);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 129);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(400, 339);
             this.pictureBox1.TabIndex = 3;
@@ -65,20 +76,123 @@
             // pictureBox2
             // 
             this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.pictureBox2.Location = new System.Drawing.Point(418, 62);
+            this.pictureBox2.Location = new System.Drawing.Point(418, 129);
             this.pictureBox2.Name = "pictureBox2";
             this.pictureBox2.Size = new System.Drawing.Size(400, 339);
             this.pictureBox2.TabIndex = 4;
             this.pictureBox2.TabStop = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "threshold min";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 41);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "threshold max";
+            // 
+            // tbThresholdMax
+            // 
+            this.tbThresholdMax.Location = new System.Drawing.Point(90, 38);
+            this.tbThresholdMax.Name = "tbThresholdMax";
+            this.tbThresholdMax.Size = new System.Drawing.Size(96, 20);
+            this.tbThresholdMax.TabIndex = 2;
+            // 
+            // tbCannyThreshold
+            // 
+            this.tbCannyThreshold.Location = new System.Drawing.Point(316, 9);
+            this.tbCannyThreshold.Name = "tbCannyThreshold";
+            this.tbCannyThreshold.Size = new System.Drawing.Size(96, 20);
+            this.tbCannyThreshold.TabIndex = 2;
+            // 
+            // tbCannyThresholdLinking
+            // 
+            this.tbCannyThresholdLinking.Location = new System.Drawing.Point(316, 35);
+            this.tbCannyThresholdLinking.Name = "tbCannyThresholdLinking";
+            this.tbCannyThresholdLinking.Size = new System.Drawing.Size(96, 20);
+            this.tbCannyThresholdLinking.TabIndex = 2;
+            // 
+            // labelx
+            // 
+            this.labelx.AutoSize = true;
+            this.labelx.Location = new System.Drawing.Point(193, 12);
+            this.labelx.Name = "labelx";
+            this.labelx.Size = new System.Drawing.Size(83, 13);
+            this.labelx.TabIndex = 6;
+            this.labelx.Text = "cannyThreshold";
+            this.labelx.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labely
+            // 
+            this.labely.AutoSize = true;
+            this.labely.Location = new System.Drawing.Point(193, 41);
+            this.labely.Name = "labely";
+            this.labely.Size = new System.Drawing.Size(117, 13);
+            this.labely.TabIndex = 7;
+            this.labely.Text = "cannyThresholdLinking";
+            // 
+            // tbMinOuterRectArea
+            // 
+            this.tbMinOuterRectArea.Location = new System.Drawing.Point(541, 6);
+            this.tbMinOuterRectArea.Name = "tbMinOuterRectArea";
+            this.tbMinOuterRectArea.Size = new System.Drawing.Size(96, 20);
+            this.tbMinOuterRectArea.TabIndex = 2;
+            // 
+            // tbMaxMarkArea
+            // 
+            this.tbMaxMarkArea.Location = new System.Drawing.Point(541, 32);
+            this.tbMaxMarkArea.Name = "tbMaxMarkArea";
+            this.tbMaxMarkArea.Size = new System.Drawing.Size(96, 20);
+            this.tbMaxMarkArea.TabIndex = 2;
+            // 
+            // labelz
+            // 
+            this.labelz.AutoSize = true;
+            this.labelz.Location = new System.Drawing.Point(418, 9);
+            this.labelz.Name = "labelz";
+            this.labelz.Size = new System.Drawing.Size(94, 13);
+            this.labelz.TabIndex = 6;
+            this.labelz.Text = "minOuterRectArea";
+            this.labelz.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(418, 38);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "maxMarkArea";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(888, 480);
+            this.ClientSize = new System.Drawing.Size(834, 480);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.labely);
+            this.Controls.Add(this.labelz);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelx);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.tbMaxMarkArea);
             this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.tbMinOuterRectArea);
+            this.Controls.Add(this.tbCannyThresholdLinking);
+            this.Controls.Add(this.tbCannyThreshold);
+            this.Controls.Add(this.tbThresholdMax);
+            this.Controls.Add(this.tbThresholdMin);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -93,9 +207,20 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox tbThresholdMin;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbThresholdMax;
+        private System.Windows.Forms.TextBox tbCannyThreshold;
+        private System.Windows.Forms.TextBox tbCannyThresholdLinking;
+        private System.Windows.Forms.Label labelx;
+        private System.Windows.Forms.Label labely;
+        private System.Windows.Forms.TextBox tbMinOuterRectArea;
+        private System.Windows.Forms.TextBox tbMaxMarkArea;
+        private System.Windows.Forms.Label labelz;
+        private System.Windows.Forms.Label label4;
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -12,6 +12,12 @@ namespace WindowsFormsApplication1
     public partial class Form1 : Form
     {
         ConcurrentQueue<Bitmap> processed = new ConcurrentQueue<Bitmap>();
+        private double maxMarkArea;
+        private double minOuterRectArea;
+        private double cannyThreshold;
+        private double cannyThresholdLinking;
+        private double binThresholdMax;
+        private double binThresholdMin;
         public Form1()
         {
             InitializeComponent();
@@ -19,11 +25,29 @@ namespace WindowsFormsApplication1
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            binThresholdMin = 253;
+            binThresholdMax = 255;
+            cannyThreshold = 200;
+            cannyThresholdLinking = 250;
+            minOuterRectArea = 5000;
+            maxMarkArea = 200;
 
+            tbThresholdMin.Text = binThresholdMin.ToString();
+            tbThresholdMax.Text = binThresholdMax.ToString();
+            tbCannyThreshold.Text = cannyThreshold.ToString();
+            tbCannyThresholdLinking.Text = cannyThresholdLinking.ToString();
+            tbMinOuterRectArea.Text = minOuterRectArea.ToString();
+            tbMaxMarkArea.Text = maxMarkArea.ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            binThresholdMin = Convert.ToDouble(tbThresholdMin.Text);
+            binThresholdMax = Convert.ToDouble(tbThresholdMax.Text);
+            cannyThreshold = Convert.ToDouble(tbCannyThreshold.Text);
+            cannyThresholdLinking = Convert.ToDouble(tbCannyThresholdLinking.Text);
+            minOuterRectArea = Convert.ToDouble(tbMinOuterRectArea.Text);
+            maxMarkArea = Convert.ToDouble(tbMaxMarkArea.Text);
             Thread processor = new Thread(new ThreadStart(work1));
             processor.Start();
         }
@@ -55,17 +79,17 @@ namespace WindowsFormsApplication1
         private Bitmap calc(Image<Bgr,Byte> My_Image){
             Image<Gray, byte> gray_image = My_Image.Convert<Gray, byte>();
             gray_image._SmoothGaussian(9);
-            gray_image.ThresholdBinaryInv(new Gray(150), new Gray(255));
+            gray_image.ThresholdBinaryInv(new Gray(binThresholdMin), new Gray(binThresholdMax));
             //gray_image = gray_image.ThresholdAdaptive(new Gray(255), Emgu.CV.CvEnum.AdaptiveThresholdType.MeanC, Emgu.CV.CvEnum.ThresholdType.Binary, 75, new Gray(10));
             //CvInvoke.BitwiseNot(gray_image, gray_image);
-            gray_image = gray_image.Canny(0, 170);
+            gray_image = gray_image.Canny(cannyThreshold, cannyThresholdLinking);
             VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
             IInputOutputArray hierarchy = null;
             CvInvoke.FindContours(gray_image, contours, hierarchy, Emgu.CV.CvEnum.RetrType.Tree, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
             int contourCount = contours.Size;
             int screenCnt = 0;
             double maxContourArea = 0;
-            Console.WriteLine("Contours: " + contourCount);
+            //Console.WriteLine("Contours: " + contourCount);
             //VectorOfVectorOfPoint foundSquares = new VectorOfVectorOfPoint();
             int biggestRect = 0;
             for (int i = 0; i < contourCount; i++)
@@ -74,54 +98,41 @@ namespace WindowsFormsApplication1
                 Rectangle rect = CvInvoke.BoundingRectangle(contour);
                 //double k = (rect.Height + 0.0) / rect.Width;
                 double area = rect.Height * rect.Width;
-                if (area > 5000 && area > maxContourArea)
+                if (area > minOuterRectArea && area > maxContourArea)
                 {
                     //CvInvoke.DrawContours(My_Image, contours, i, new MCvScalar(0, 255, 0), 1, LineType.FourConnected, hierarchy, 0, new Point());
                     maxContourArea = area;
                     biggestRect = i;
                     screenCnt++;
                     //foundSquares.Push(contour);
+                }else if(area < maxMarkArea){
+                    gray_image.Draw(rect, new Gray(255), 1);
+                    //My_Image.Draw(rect, new Bgr(255, 0, 0), 2);
                 }
             }
-            Console.WriteLine("Found: " + screenCnt);
+            //Console.WriteLine("Found: " + screenCnt);
             if (contourCount > 0)
             {
                 VectorOfPointF approx = new VectorOfPointF();
                 CvInvoke.ApproxPolyDP(contours[biggestRect], approx, CvInvoke.ArcLength(contours[biggestRect], true) * 0.02, true);
                 if (approx.Size == 4)
                 {
-                    PointF[] sortedApproxArr = sort(approx.ToArray());
-                    VectorOfPointF sortedApprox = new VectorOfPointF(sortedApproxArr);
-                    double rotAngle = 0;
-                    if (sortedApproxArr[0].X > sortedApproxArr[3].X)
-                    {
-                        //should rotate CCW
-                        rotAngle = Math.Atan2((sortedApproxArr[2].Y - sortedApproxArr[1].Y), (sortedApproxArr[2].X - sortedApproxArr[1].X));
-                        //RotationMatrix2D rotMatrix = new RotationMatrix2D(sortedApproxArr[0], rotAngle, 1);
-                        //rotMatrix.RotatePoints(sortedApproxArr);
-                    }
-                    else if (sortedApproxArr[0].X < sortedApproxArr[3].X)
-                    {
-                        rotAngle = Math.Atan2((sortedApproxArr[3].Y - sortedApproxArr[0].Y), (sortedApproxArr[3].X - sortedApproxArr[0].X));
-                    }
-                    Rectangle orgBoundary = CvInvoke.BoundingRectangle(sortedApprox);
-                    PointF orgBoundaryCenter = new PointF(orgBoundary.Width/2, orgBoundary.Height/2);
-                    RotatedRect rotatedRect = new RotatedRect(orgBoundaryCenter, orgBoundary.Size, Convert.ToSingle(rotAngle));
-                    Rectangle rotatedBoundary = rotatedRect.MinAreaRect();
-                    My_Image.Draw(orgBoundary, new Bgr(0, 0, 255), 1);
+                    Rectangle roi = CvInvoke.BoundingRectangle(approx);
+                    My_Image.Draw(roi, new Bgr(0, 0, 255), 2);
+                    int shift = 0;
                     VectorOfPointF dst = new VectorOfPointF(new PointF[] { 
-                        new PointF(rotatedBoundary.Left, rotatedBoundary.Top),
-                        new PointF(rotatedBoundary.Right, rotatedBoundary.Top),
-                        new PointF(rotatedBoundary.Left, rotatedBoundary.Bottom),
-                        new PointF(rotatedBoundary.Right, rotatedBoundary.Bottom)
+                        new PointF(roi.Left-shift, roi.Top-shift),
+                        new PointF(roi.Right+shift, roi.Top-shift),
+                        new PointF(roi.Left-shift, roi.Bottom+shift),
+                        new PointF(roi.Right+shift, roi.Bottom+shift)
                     });
-                    Mat perspectiveMat = CvInvoke.GetPerspectiveTransform(sort(rotatedRect.GetVertices()), sort(dst.ToArray()));
-                    Mat transformed = new Emgu.CV.Mat(My_Image.Rows, My_Image.Cols, DepthType.Cv8U, 2);
-                    CvInvoke.WarpPerspective(My_Image, transformed, perspectiveMat, My_Image.Size);
+                    var sortedApproxArr = sort(approx.ToArray());
+                    var sortedDstArr = sort(dst.ToArray());
+                    Mat perspectiveMat = CvInvoke.GetPerspectiveTransform(sortedApproxArr, sortedDstArr);
+                    Mat transformed = new Emgu.CV.Mat(gray_image.Rows, gray_image.Cols, DepthType.Cv8U, 1);
+                    CvInvoke.WarpPerspective(gray_image, transformed, perspectiveMat, gray_image.Size, Inter.Linear, Warp.Default, BorderType.Wrap);
                     //My_Image.DrawPolyline(approx.ToArray(), true, new Bgr(0, 255, 155));
-                    Image<Bgr, byte> warped = transformed.ToImage<Bgr, byte>();
-                    warped.Draw(rotatedBoundary, new Bgr(0, 0, 255), 2);
-                    pictureBox2.Image = warped.ToBitmap();
+                    pictureBox2.BackgroundImage = transformed.ToImage<Bgr, byte>().GetSubRect(roi).ToBitmap();
                 }
             }
 
@@ -132,13 +143,38 @@ namespace WindowsFormsApplication1
         }
         private PointF[] sort(PointF[] points)
         {
-            double[] angles = new double[points.Length];
             for (int i = 0; i < points.Length; i++)
             {
-                angles[i] = Math.Atan2(points[i].Y, points[i].X);
+                for (int j = 0; j < points.Length-1; j++)
+                {
+                    if (points[i].Y < points[j].Y)
+                    {
+                        swap(points, i, j);
+                    }
+                }
             }
-            Array.Sort(angles, points);
+            if (points[0].X > points[1].X)
+            {
+                swap(points, 0, 1);
+            }
+
+            if (points[2].X < points[3].X)
+            {
+                swap(points, 2, 3);
+            }
             return points;
+        }
+
+        private static void swap<T>(T[] points, int i, int j)
+        {
+            T temp = points[i];
+            points[i] = points[j];
+            points[j] = temp;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
