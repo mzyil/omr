d5baabd88905d29ee97d125a7fad7c4ddcc2d93a
mzyil <mzyil12@gmail.com>
2016-09-15 21:07:40 +0300

    rotation test (not working well)

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -61,7 +61,7 @@ namespace WindowsFormsApplication1
             gray_image = gray_image.Canny(0, 170);
             VectorOfVectorOfPoint contours = new VectorOfVectorOfPoint();
             IInputOutputArray hierarchy = null;
-            CvInvoke.FindContours(gray_image, contours, hierarchy, Emgu.CV.CvEnum.RetrType.External, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
+            CvInvoke.FindContours(gray_image, contours, hierarchy, Emgu.CV.CvEnum.RetrType.Tree, Emgu.CV.CvEnum.ChainApproxMethod.ChainApproxSimple);
             int contourCount = contours.Size;
             int screenCnt = 0;
             double maxContourArea = 0;
@@ -86,51 +86,42 @@ namespace WindowsFormsApplication1
             Console.WriteLine("Found: " + screenCnt);
             if (contourCount > 0)
             {
-                //CvInvoke.DrawContours(My_Image, contours, biggestRect, new MCvScalar(0, 255, 0), 1, LineType.FourConnected, hierarchy, 0, new Point());
-                //My_Image.Draw(CvInvoke.BoundingRectangle(contours[biggestRect]), new Bgr(0, 255, 0), 1);
-                /*Rectangle roi = CvInvoke.BoundingRectangle(contours[biggestRect]);
-                VectorOfPoint src = new VectorOfPoint(new Point[] { 
-                    new Point(roi.Left, roi.Top),
-                    new Point(roi.Right, roi.Top),
-                    new Point(roi.Left, roi.Bottom),
-                    new Point(roi.Right, roi.Bottom)
-                });
-                VectorOfPoint dst = new VectorOfPoint(new Point[] { 
-                    new Point(0, 0),
-                    new Point(0, roi.Width),
-                    new Point(0, roi.Height),
-                    new Point(roi.Height, roi.Width)
-                });
-                Mat perspectiveMat = CvInvoke.GetPerspectiveTransform(src, dst);
-                Matrix<Bgr> perspectiveMatrix = null;
-                perspectiveMat.ConvertTo(perspectiveMatrix, DepthType.Default);
-                return My_Image.WarpPerspective<Bgr>(perspectiveMatrix, Inter.Nearest, Warp.FillOutliers, BorderType.Constant, new Bgr(0,255,0)).ToBitmap();
-                using (Mat matrix = CvInvoke.GetPerspectiveTransform(contours[biggestRect], dst))
-                {
-                    using (Mat cutImagePortion = new Mat())
-                    {
-                        CvInvoke.WarpPerspective(My_Image, cutImagePortion, matrix, roi.Size, Inter.Cubic);
-                        return cutImagePortion.ToImage<Bgr, byte>().ToBitmap();
-                    }
-                }*/
-                
                 VectorOfPointF approx = new VectorOfPointF();
                 CvInvoke.ApproxPolyDP(contours[biggestRect], approx, CvInvoke.ArcLength(contours[biggestRect], true) * 0.02, true);
                 if (approx.Size == 4)
                 {
-                    Rectangle roi = CvInvoke.BoundingRectangle(approx);
-                    My_Image.Draw(roi, new Bgr(0, 0, 255), 2);
+                    PointF[] sortedApproxArr = sort(approx.ToArray());
+                    VectorOfPointF sortedApprox = new VectorOfPointF(sortedApproxArr);
+                    double rotAngle = 0;
+                    if (sortedApproxArr[0].X > sortedApproxArr[3].X)
+                    {
+                        //should rotate CCW
+                        rotAngle = Math.Atan2((sortedApproxArr[2].Y - sortedApproxArr[1].Y), (sortedApproxArr[2].X - sortedApproxArr[1].X));
+                        //RotationMatrix2D rotMatrix = new RotationMatrix2D(sortedApproxArr[0], rotAngle, 1);
+                        //rotMatrix.RotatePoints(sortedApproxArr);
+                    }
+                    else if (sortedApproxArr[0].X < sortedApproxArr[3].X)
+                    {
+                        rotAngle = Math.Atan2((sortedApproxArr[3].Y - sortedApproxArr[0].Y), (sortedApproxArr[3].X - sortedApproxArr[0].X));
+                    }
+                    Rectangle orgBoundary = CvInvoke.BoundingRectangle(sortedApprox);
+                    PointF orgBoundaryCenter = new PointF(orgBoundary.Width/2, orgBoundary.Height/2);
+                    RotatedRect rotatedRect = new RotatedRect(orgBoundaryCenter, orgBoundary.Size, Convert.ToSingle(rotAngle));
+                    Rectangle rotatedBoundary = rotatedRect.MinAreaRect();
+                    My_Image.Draw(orgBoundary, new Bgr(0, 0, 255), 1);
                     VectorOfPointF dst = new VectorOfPointF(new PointF[] { 
-                        new PointF(roi.Left, roi.Top),
-                        new PointF(roi.Right, roi.Top),
-                        new PointF(roi.Left, roi.Bottom),
-                        new PointF(roi.Right, roi.Bottom)
+                        new PointF(rotatedBoundary.Left, rotatedBoundary.Top),
+                        new PointF(rotatedBoundary.Right, rotatedBoundary.Top),
+                        new PointF(rotatedBoundary.Left, rotatedBoundary.Bottom),
+                        new PointF(rotatedBoundary.Right, rotatedBoundary.Bottom)
                     });
-                    Mat perspectiveMat = CvInvoke.GetPerspectiveTransform(sort(approx.ToArray()), sort(dst.ToArray()));
-                    Mat transformed = new Emgu.CV.Mat(My_Image.Rows, My_Image.Cols, DepthType.Cv8U, 3);
+                    Mat perspectiveMat = CvInvoke.GetPerspectiveTransform(sort(rotatedRect.GetVertices()), sort(dst.ToArray()));
+                    Mat transformed = new Emgu.CV.Mat(My_Image.Rows, My_Image.Cols, DepthType.Cv8U, 2);
                     CvInvoke.WarpPerspective(My_Image, transformed, perspectiveMat, My_Image.Size);
                     //My_Image.DrawPolyline(approx.ToArray(), true, new Bgr(0, 255, 155));
-                    pictureBox2.BackgroundImage = transformed.ToImage<Bgr, byte>().GetSubRect(roi).ToBitmap();
+                    Image<Bgr, byte> warped = transformed.ToImage<Bgr, byte>();
+                    warped.Draw(rotatedBoundary, new Bgr(0, 0, 255), 2);
+                    pictureBox2.Image = warped.ToBitmap();
                 }
             }
 
